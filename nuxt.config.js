735a8bddf82bdcc72c2fb74f80aab3fafb86dea2
Marian Adamus <cmanar0@gmail.com>
2024-03-18 12:35:39 +0100

    turn ssr on

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,18 @@
 export default defineNuxtConfig({
+  ssr: true, // Explicitly enable SSR
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      datoCmsToken: process.env.DATO_CMS_TOKEN
-    }
+      datoCmsToken: process.env.DATO_CMS_TOKEN,
+    },
   },
   css: [
-    '~/assets/css/main.css' // Path to your global CSS file
+    "~/assets/css/main.css", // Path to your global CSS file
   ],
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1'
-    }
-  }
-})
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
+  },
+});
